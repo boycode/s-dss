ff2143a11e5131d401e7b5f70ca06876ea3111ef
nanospeck <watercooler>
2015-05-17 19:27:24 +0530

    Spring and MonogoDB working copy

---
+++ dataflue-ui/SpringMongoDB/src/main/java/com/dataflue/config/SpringMongoConfig1.java
@@ -0,0 +1,28 @@
+package com.dataflue.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import com.mongodb.MongoClient;
+
+@Configuration
+public class SpringMongoConfig1 {
+
+	public @Bean
+	MongoDbFactory mongoDbFactory() throws Exception {
+		return new SimpleMongoDbFactory(new MongoClient(), "yourdb");
+	}
+
+	public @Bean
+	MongoTemplate mongoTemplate() throws Exception {
+		
+		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+				
+		return mongoTemplate;
+		
+	}
+
+}
---
+++ dataflue-ui/SpringMongoDB/src/main/java/com/dataflue/core/App.java
@@ -0,0 +1,61 @@
+package com.dataflue.core;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+
+import com.dataflue.config.SpringMongoConfig;
+import com.dataflue.model.User;
+//import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		// For XML
+		//ApplicationContext ctx = new GenericXmlApplicationContext("SpringConfig.xml");
+
+		// For Annotation
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringMongoConfig.class);
+		MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
+
+		User user = new User("mkyong", "password123");
+
+		// save
+		mongoOperation.save(user);
+
+		// now user object got the created id.
+		System.out.println("1. user : " + user);
+
+		// query to search user
+		Query searchUserQuery = new Query(Criteria.where("username").is("mkyong"));
+
+		// find the saved user again.
+		User savedUser = mongoOperation.findOne(searchUserQuery, User.class);
+		System.out.println("2. find - savedUser : " + savedUser);
+
+		// update password
+		mongoOperation.updateFirst(searchUserQuery, Update.update("password", "new password"),
+				User.class);
+
+		// find the updated user object
+		User updatedUser = mongoOperation.findOne(
+				new Query(Criteria.where("username").is("mkyong")), User.class);
+
+		System.out.println("3. updatedUser : " + updatedUser);
+
+		// delete
+		mongoOperation.remove(searchUserQuery, User.class);
+
+		// List, it should be empty now.
+		List<User> listUser = mongoOperation.findAll(User.class);
+		System.out.println("4. Number of user = " + listUser.size());
+
+	}
+
+}
