e3a4c5b42c0449d8cf9e9fa29d0c7f41e1436833
git@github.com:boycode/s-dss.git <rajivkris@gmail.com>
2015-06-21 21:34:37 +0530

    Enable second-level cache for dataflue persistence layer

--- dataflue-cip/dataflue-insight-persistence/src/test/java/com/dataflue/cip/persistence/ConsumerRepositoryTest.java
+++ dataflue-cip/dataflue-insight-persistence/src/test/java/com/dataflue/cip/persistence/ConsumerRepositoryTest.java
@@ -1,12 +1,10 @@
 package com.dataflue.cip.persistence;
 
-import java.util.List;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
-import com.dataflue.cip.persistence.entities.Consumer;
-import com.dataflue.cip.persistence.repository.ConsumerRepository;
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.github.springtestdbunit.annotation.ExpectedDatabase;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,9 +15,13 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.util.StopWatch;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import com.dataflue.cip.persistence.entities.Consumer;
+import com.dataflue.cip.persistence.repository.ConsumerRepository;
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.ExpectedDatabase;
 
 /**
  * Inetegration Suite to  Test the DB 
@@ -38,6 +40,8 @@ public class ConsumerRepositoryTest {
 	@Autowired
 	private ConsumerRepository repository;
 	
+	private StopWatch stopWatch = new StopWatch();
+	
 	/**
 	 * Minimal Integration Test - Connection - Hikari Source
 	 */
@@ -50,20 +54,42 @@ public class ConsumerRepositoryTest {
 	/**
 	 * Sample Test DB if needed - for 
 	 */
-	//@Test
-	@DatabaseSetup("classpath:sampledata.xml")
+	@Test
+	@DatabaseSetup("/sampledata.xml")
 	public void setUpDatabase_ShouldValidateAnEntry(){
-		List<Consumer> returnConsumerList = (List)repository.findAll();
-		assertEquals(2, returnConsumerList.size());
-		assertEquals("Flipkart", returnConsumerList.get(0).getConsumerName());
+		List<Consumer> returnConsumerList = (List<Consumer>)repository.findAll();
+		assertEquals(16, returnConsumerList.size());
+		assertEquals("Rajiv", returnConsumerList.get(0).getConsumerName());
 		
 	}
+	
+	/**
+	 * Test time taken in ms for each DB fetch 
+	 */
+	@Test
+    public void testRetrieveAll() throws Exception {
+        stopWatch.start();
+        repository.findAllConsumers();
+        stopWatch.stop();
+        System.out.println("time to retrieve all: " + stopWatch.getLastTaskTimeMillis() + "ms");
+    }
+     
+	/**
+	 * Test ehcache by calling testretriveAll()
+	 *  multiple times and check time taken for each DB fetch
+	 */
+    @Test
+    public void testMultipleRetrieveAll() throws Exception {
+        for (int i=0;i<10;i++) {
+            testRetrieveAll();
+        }
+    }
 	
 	/**
 	 * 
 	 * @throws Exception
 	 */
-	@Test
+	//@Test
 	@DatabaseSetup("classpath:sampledata.xml")
 	@ExpectedDatabase("classpath:expecteddata.xml")
 	public void testRemove() throws Exception {
