98bc6fbd431ba5d14b4d0ea6ebe48c3241b092c4
git@github.com:boycode/s-dss.git <rajivkris@gmail.com>
2015-06-21 21:31:48 +0530

    Enable second-level ehcaching for dataflue persistence layer

--- dataflue-cip/dataflue-insight-persistence/src/main/java/com/dataflue/cip/persistence/entities/Consumer.java
+++ dataflue-cip/dataflue-insight-persistence/src/main/java/com/dataflue/cip/persistence/entities/Consumer.java
@@ -3,20 +3,29 @@
  */
 package com.dataflue.cip.persistence.entities;
 
+import javax.persistence.Cacheable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 /**
  * @author Vishnu Sankar
  *
  */
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 @Entity
 @Table(name = "CIP_CONSUMER")
 public class Consumer {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
     @Column(name = "consumer_cid")
     private int consumerId;
 	
--- dataflue-cip/dataflue-insight-persistence/src/main/java/com/dataflue/cip/persistence/repository/ConsumerRepository.java
+++ dataflue-cip/dataflue-insight-persistence/src/main/java/com/dataflue/cip/persistence/repository/ConsumerRepository.java
@@ -2,7 +2,10 @@ package com.dataflue.cip.persistence.repository;
 
 import java.util.List;
 
+import javax.persistence.QueryHint;
+
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -16,5 +19,10 @@ import com.dataflue.cip.persistence.entities.Consumer;
 public interface ConsumerRepository extends CrudRepository<Consumer,Integer>{
 
 	    @Query("Select t FROM Consumer t WHERE LOWER(t.consumerName) LIKE LOWER(CONCAT('%', :searchTerm, '%'))")
+	    @QueryHints({ @QueryHint(name = "org.hibernate.cacheable", value ="true") })
 	    public List<Consumer> search(@Param("searchTerm") String searchTerm);
+	    
+	    @Query("Select t FROM Consumer t")
+	    @QueryHints({ @QueryHint(name = "org.hibernate.cacheable", value ="true") })
+	    public List<Consumer> findAllConsumers();
 }
